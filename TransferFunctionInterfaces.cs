48548728c8ab7d75ce9f125bb25995beaa51aefe
alexeyvale <alexeyvale.soc@yandex.ru>
2014-10-13 17:30:07 +0400

    Добавлены интерфейсы для передаточной функции и множества

---
+++ TransferFunctionInterfaces.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleLang
+{
+    public interface IndexedSet<T>
+    {
+        T Get(int index);
+        void Set(int index, T value);
+    }
+
+    public class BitSet : IndexedSet<bool>
+    {
+        private bool[] Elems;
+
+        public BitSet(int size)
+        {
+            Elems = new bool[size];
+        }
+
+        public bool Get(int index)
+        {
+            return Elems[index];
+        }
+
+        public void Set(int index, bool value)
+        {
+            Elems[index] = value;
+        }
+    }
+
+    public interface TransferFunction<T>
+    {
+        T Transfer(T input);
+    }
+
+    interface SetTransferFunction<S>:TransferFunction<IndexedSet<S>>
+    {}
+
+    //class ConcreteTransferFunctionWithBitSet
+
+}
