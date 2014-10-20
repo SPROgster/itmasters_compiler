15f9095cb4a6e4fb899a614a8e2e2e6260e053d9
alexeyvale <alexeyvale.soc@yandex.ru>
2014-10-20 16:59:49 +0400

    Реализована первая версия интерфейса передаточной функции и интерфейс для In и Out (IndexedSet), также реализован класс BitSet, использование которого предполагается при решении задачи о достигающих определениях.

--- Optimizations/TransferFunctionInterfaces.cs
+++ Optimizations/TransferFunctionInterfaces.cs
@@ -10,15 +10,24 @@ namespace SimpleLang
     {
         T Get(int index);
         void Set(int index, T value);
+
+        IndexedSet<T> Intersect(IndexedSet<T> b);
+        IndexedSet<T> Union(IndexedSet<T> b);
+        IndexedSet<T> Subtract(IndexedSet<T> b);
     }
 
-    public class BitSet : IndexedSet<bool>
+    public class BitSet : IndexedSet<bool>, ICloneable
     {
         private bool[] Elems;
 
         public BitSet(int size)
         {
             Elems = Enumerable.Repeat(false, size).ToArray();
+        }
+
+        private BitSet(bool[] elems)
+        {
+            Elems = (bool[])elems.Clone();
         }
 
         public bool Get(int index)
@@ -30,17 +39,34 @@ namespace SimpleLang
         {
             Elems[index] = value;
         }
-    }
+
+        public IndexedSet<bool> Intersect(IndexedSet<bool> b)
+        {
+            return new BitSet(Elems.Zip(((BitSet)b).Elems, (f, s) => f && s).ToArray());
+        }
+
+        public IndexedSet<bool> Union(IndexedSet<bool> b)
+        {
+            return new BitSet(Elems.Zip(((BitSet)b).Elems, (f, s) => f || s).ToArray());
+        }
+
+        public IndexedSet<bool> Subtract(IndexedSet<bool> b)
+        {
+            return new BitSet(Elems.Zip(((BitSet)b).Elems, (f,s) => s ? false : f).ToArray());
+        }
+    
+        public object Clone()
+        {
+            return new BitSet(Elems);
+        }
+}
 
     public interface TransferFunction<T>
     {
         T Transfer(T input);
     }
 
-    interface SetTransferFunction<S>:TransferFunction<IndexedSet<S>>
-    {}
-
-    //public class ConcreteTransferFunctionWithBitSet:SetTransferFunction<BitSet>
+    //public class MyCoolTransferFunctionWithBitSet:TransferFunction<BitSet>
     //{}
 
 }
