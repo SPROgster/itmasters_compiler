4fc38e5641f0e7ff15d77bfa56218766d23fc841
unknown <student@r315-4.mmcs.sfedu.ru>
2014-10-27 17:47:12 +0400

    Add CleanDead helper class

---
+++ CleanDead.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiddleEnd
+{
+    using BaseBlock = LinkedList<CodeLine>;
+
+    public class CleanDead
+    {
+        public static BaseBlock cleanBlock(BaseBlock block)
+        {
+            return null;
+        }
+    }
+}
