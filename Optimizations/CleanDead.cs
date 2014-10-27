4c6694581b160f0c2f87e149f27ad7abeb0d68d6
unknown <student@r315-4.mmcs.sfedu.ru>
2014-10-27 18:24:23 +0400

    Реализация класса CleanDead

---
+++ Optimizations/CleanDead.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using MiddleEnd;
+using DefUse;
+
+namespace CleanDead
+{
+    using BaseBlock = LinkedList<CodeLine>;
+
+    public class CleanDead
+    {
+        public static void cleanBlock(BaseBlock block)
+        {
+            var list = DeadOrAlive.GenerateDefUse(new List<CodeLine>(block));
+            var p = block.First;
+
+            foreach (Tuple<string, string, int> def in list)
+            {
+                if (!DeadOrAlive.IsAlive(block, def.Item1, def.Item3))
+                    block.Remove(p);
+                p = p.Next;
+            }
+        }
+    }
+}
