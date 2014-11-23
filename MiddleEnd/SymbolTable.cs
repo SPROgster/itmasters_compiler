92e139896e26a690efbd8e861f94df05eade30c8
vdmkrchkn <vdmkrchkn@gmail.com>
2014-11-23 22:17:37 +0300

    для последующего использования в оптимизациях OpResultType, NextTemp перемещены в SymbolTable

--- MiddleEnd/SymbolTable.cs
+++ MiddleEnd/SymbolTable.cs
@@ -54,5 +54,49 @@ namespace SimpleLang.MiddleEnd
                 default         : return CType.None;
             }
         }
+
+        /// <summary>
+        /// Определяет какого типа должен быть результат операции по двум операторам
+        /// </summary>
+        /// <param name="op1">Первый операнд</param>
+        /// <param name="op2">Второй операнд</param>
+        /// <param name="op">Операция</param>
+        /// <returns>CType результата операции</returns>
+        public static CType OpResultType(CType op1, CType op2, BinOpType op)
+        {
+            if (op2 == CType.None)
+                return op1;
+
+            if (op == BinOpType.Equal || op == BinOpType.GEqual || op == BinOpType.Greater ||
+                op == BinOpType.LEqual || op == BinOpType.Less || op == BinOpType.NEqual)
+                return CType.Bool;
+
+            if ((op1 == CType.Double) || (op2 == CType.Double))
+                return CType.Double;
+
+            if ((op1 == CType.Float) || (op2 == CType.Float))
+                return CType.Float;
+
+            if ((op1 == CType.Int) || (op2 == CType.Int))
+                return CType.Int;
+
+            if ((op1 == CType.Bool) || (op2 == CType.Bool))
+                return CType.Bool;
+
+            return CType.None;
+        }
+
+        static int TempCounter = 0;
+        const string TempName = "_t";
+        /// <summary>
+        /// Возвращает следующую неиспользуемую временную переменную
+        /// </summary>
+        /// <returns>следующая неиспользуемая временная переменная</returns>
+        public static string NextTemp()
+        {
+            while (SymbolTable.Contains(TempName + TempCounter))
+                TempCounter++;
+            return TempName + TempCounter++;
+        }
     }
 }
