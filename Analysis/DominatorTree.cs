12ec76ba269ba3245417a994bd5a8d1f176eefe8
unknown <student@r315-4.mmcs.sfedu.ru>
2014-11-24 18:44:14 +0400

    Начата работа над построением дерева доминаторов

---
+++ Analysis/DominatorTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using SimpleLang.MiddleEnd;
+
+namespace SimpleLang.Analysis
+{
+    using BlockSet = SetAdapter<BaseBlock>;
+
+    public class DominatorTree
+    {
+        private class EmptyContext: Context<BaseBlock>
+        {
+            public EmptyContext(ControlFlowGraph cfg)
+                :base(cfg)
+            { }
+        }
+
+        private class DominatorsAlgorithm : TopDownAlgorithm<BaseBlock, EmptyContext, BlockSet>
+        {
+            public class DominatorsTransferFunction: InfoProvidedTransferFunction<BaseBlock, BlockSet>
+            {
+                public DominatorsTransferFunction(BaseBlock info)
+                    : base(info)
+                { }
+
+                public override BlockSet Transfer(BlockSet input)
+                {
+                    var Result = (BlockSet)input.Clone();
+                    Result.Add(Info);
+                    return Result;
+                }
+            }
+
+            public DominatorsAlgorithm(ControlFlowGraph cfg)
+                : base(cfg)
+            {
+                foreach (BaseBlock bl in cfg.GetBlocks())
+                {
+                    In[bl] = new BlockSet();
+                    Out[bl] = new BlockSet();
+                    Func[bl] = new DominatorsTransferFunction(bl);
+                }
+            }
+
+            public override Tuple<Dictionary<BaseBlock, BlockSet>, Dictionary<BaseBlock, BlockSet>> Apply()
+            {
+                BlockSet AllBlocks = new BlockSet();
+                foreach (BaseBlock bl in this.Cont.Blocks)
+                    AllBlocks.Add(bl);
+                return base.Apply(new BlockSet(Cont.Start), AllBlocks, AllBlocks, BlockSet.Intersect);
+            }
+        }
+
+        public DominatorTree(ControlFlowGraph cfg)
+        {
+
+        }
+    }
+}
