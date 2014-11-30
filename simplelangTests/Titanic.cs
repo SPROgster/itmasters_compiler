28b73f94194187f5a58c22a2f6d1b5177ce9b3d4
Oleg <yurin.oleg.i@gmail.com>
2014-11-30 19:47:40 +0300

    Косячно залил тесты. Забыл титаник и файл проекта

---
+++ simplelangTests/Titanic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleLang.Visitors;
+using SimpleScanner;
+using SimpleParser;
+using System.IO;
+using SimpleLang.MiddleEnd;
+
+namespace simplelangTests
+{
+    [TestClass]
+    public class Titanic
+    {
+        [TestMethod]
+        public void TestGraphBBL()
+        {
+            string FileName = @"..\..\Test3.txt";
+            string Text = File.ReadAllText(FileName);
+            Scanner scanner = new Scanner();
+            scanner.SetSource(Text, 0);
+            Parser parser = new Parser(scanner);
+            var b = parser.Parse();
+            var sne = new CheckVariablesVisitor();
+            parser.root.Visit(sne);
+            GenCodeVisitor gcv = new GenCodeVisitor();
+            parser.root.Visit(gcv);
+            gcv.RemoveEmptyLabels();
+            ControlFlowGraph CFG = new ControlFlowGraph(gcv.Code);
+            Assert.AreEqual(10, CFG.GetBlocks().Count);
+        }
+    }
+}
