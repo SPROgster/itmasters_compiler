060eed9bb7a52658a9d4e13a5373aa0fff59000b
Oleg <yurin.oleg.i@gmail.com>
2014-12-08 11:44:59 +0300

    Генерация Gen и Kill теперь не учитывает временные переменные. Написан тест для генератора Gen и Kill.

--- simplelangTests/Beda.cs
+++ simplelangTests/Beda.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SimpleScanner;
-using SimpleParser;
-using System.IO;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCompiler;
+using SimpleLang.Analysis;
 using SimpleLang.MiddleEnd;
 using SimpleLang.Visitors;
-using SimpleLang.Analysis;
-using SimpleCompiler;
+using SimpleParser;
+using SimpleScanner;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace simplelangTests
 {
@@ -33,13 +33,43 @@ namespace simplelangTests
             List<CodeLine> bl = new List<CodeLine>(CFG.GetBlocks().First.Next.Value.Code);
             for (int i = 0; i < bl.Count; i++)
             {
-                Console.WriteLine(bl[i]+ " "+ DeadOrAlive.IsDead(CFG.GetBlocks().First.Next.Value, bl[i].First, i));
+                Console.WriteLine(bl[i] + " " + DeadOrAlive.IsDead(CFG.GetBlocks().First.Next.Value, bl[i].First, i));
             }
-              
+
             bool alive =
                 DeadOrAlive.IsAliveBeforeLine(CFG.GetBlocks().First.Next.Value, "i", 0) &&
                 DeadOrAlive.IsAliveAfterLine(CFG.GetBlocks().First.Next.Value, "i", 0);
             Assert.IsFalse(alive);
+        }
+
+        [TestMethod]
+        public void GenKill_2_5()
+        {
+            string FileName = @"..\..\Test3.txt";
+            string Text = File.ReadAllText(FileName);
+            Scanner scanner = new Scanner();
+            scanner.SetSource(Text, 0);
+            Parser parser = new Parser(scanner);
+            var b = parser.Parse();
+            var sne = new CheckVariablesVisitor();
+            parser.root.Visit(sne);
+            GenCodeVisitor gcv = new GenCodeVisitor();
+            parser.root.Visit(gcv);
+            gcv.RemoveEmptyLabels();
+            ControlFlowGraph CFG = new ControlFlowGraph(gcv.Code);
+            SimpleCompilerMain.PrintCFG(CFG);
+            KillGenContext egc = new KillGenContext(CFG);            
+            foreach (var item in CFG.GetBlocks())
+            {
+                Console.WriteLine(item);
+                Console.WriteLine(egc.Gen(item).ToString().Replace("True", "1").Replace("False", "0"));
+                Console.WriteLine(egc.Kill(item).ToString().Replace("True", "1").Replace("False", "0"));
+                Console.WriteLine();
+            }
+            BitSet resultgen = egc.Gen(CFG.GetBlocks().First.Next.Value);
+            BitSet rightgen = new BitSet(new bool[] { true, true, true, false, false, false, false });
+            Assert.IsTrue(resultgen.Equals(rightgen));
+            
         }
     }
 }
