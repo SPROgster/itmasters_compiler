a366eab6ccc13c270f83a15c8f0f0f34f0dd44e3
Oleg <yurin.oleg.i@gmail.com>
2014-12-08 12:05:10 +0300

    Тест достигающих определений.

--- simplelangTests/Oblachko.cs
+++ simplelangTests/Oblachko.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SimpleLang.Visitors;
-using SimpleScanner;
-using SimpleParser;
-using System.IO;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCompiler;
+using SimpleLang.Analysis;
 using SimpleLang.MiddleEnd;
 using SimpleLang.Optimizations;
+using SimpleLang.Visitors;
+using SimpleParser;
+using SimpleScanner;
+using System;
 using System.Collections.Generic;
-using SimpleCompiler;
+using System.IO;
 
 namespace simplelangTests
 {
@@ -33,9 +34,39 @@ namespace simplelangTests
             Fold.fold(ref gcv.Code);
             SimpleCompilerMain.PrintCFG(CFG);
             List<CodeLine> myList = new List<CodeLine>(gcv.Code);
-            Assert.AreEqual("5",myList[3].Second);
+            Assert.AreEqual("5", myList[3].Second);
             Assert.AreEqual("-2", myList[4].Second);
             Assert.AreEqual("4", myList[5].Second);
+        }
+        [TestMethod]
+        public void RDA_2_6()
+        {
+            string FileName = @"..\..\Test3.txt";
+            string Text = File.ReadAllText(FileName);
+            Scanner scanner = new Scanner();
+            scanner.SetSource(Text, 0);
+            Parser parser = new Parser(scanner);
+            var b = parser.Parse();
+            var sne = new CheckVariablesVisitor();
+            parser.root.Visit(sne);
+            GenCodeVisitor gcv = new GenCodeVisitor();
+            parser.root.Visit(gcv);
+            gcv.RemoveEmptyLabels();
+            ControlFlowGraph CFG = new ControlFlowGraph(gcv.Code);
+            Console.WriteLine();
+            ReachingDefsAlgorithm RDA = new ReachingDefsAlgorithm(CFG);
+            var RDAResult = RDA.Apply();
+            foreach (var block in RDAResult.Item1.Keys)
+                if (block != CFG.GetStart() && block != CFG.GetEnd())
+                {
+                    Console.WriteLine(block);
+                    Console.WriteLine("In:\t" + RDAResult.Item1[block].ToString().Replace("True", "1").Replace("False", "0"));
+                    Console.WriteLine("Out:\t" + RDAResult.Item2[block].ToString().Replace("True", "1").Replace("False", "0"));
+                }
+            List<BaseBlock> bl = new List<BaseBlock>(CFG.GetBlocks());
+            BitSet resultin = RDAResult.Item1[bl[3]];
+            BitSet rightin = new BitSet(new bool[] { true, true, true, false, true, true, true });
+            Assert.IsTrue(resultin.Equals(rightin));
         }
     }
 }
