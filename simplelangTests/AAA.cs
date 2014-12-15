afd49ceeb8b55ad4e162ae7db27c7cd8ec5d1757
Pavel Artsishevskiy <student@r315-4.mmcs.sfedu.ru>
2014-12-15 18:05:10 +0400

    Добавлен тест для передаточной функции в виде формулы

--- simplelangTests/AAA.cs
+++ simplelangTests/AAA.cs
@@ -8,6 +8,7 @@ using SimpleLang.MiddleEnd;
 using SimpleLang.Optimizations;
 using SimpleCompiler;
 using SimpleLang.Analysis;
+using System.Collections.Generic;
 
 namespace simplelangTests
 {
@@ -34,6 +35,33 @@ namespace simplelangTests
             cl.Optimize(CFG.GetBlocks().First.Next.Value);
             SimpleCompilerMain.PrintCFG(CFG);
             Assert.AreEqual(6, CFG.GetBlocks().First.Next.Value.Code.Count);
+        }
+
+        [TestMethod]
+        public void FormulaTF_2_4()
+        {
+            string FileName = @"..\..\Test2.txt";
+            string Text = File.ReadAllText(FileName);
+            Scanner scanner = new Scanner();
+            scanner.SetSource(Text, 0);
+            Parser parser = new Parser(scanner);
+            var b = parser.Parse();
+            var sne = new CheckVariablesVisitor();
+            parser.root.Visit(sne);
+            GenCodeVisitor gcv = new GenCodeVisitor();
+            parser.root.Visit(gcv);
+            gcv.RemoveEmptyLabels();
+            ControlFlowGraph CFG = new ControlFlowGraph(gcv.Code);
+
+            KillGenContext cont = new KillGenContext(CFG);
+            List<Tuple<BitSet, BitSet>> tupl = new List<Tuple<BitSet, BitSet>>();
+            foreach (var line in CFG.GetBlocks().First.List)
+            {
+                tupl.Add(new Tuple<BitSet, BitSet>(cont.Gen(line), cont.Kill(line)));
+            }
+            FormulaTransferFunction tf = new FormulaTransferFunction(tupl);
+            BitSet input = new BitSet(cont.Count);
+            tf.Transfer(input);
         }
     }
 }
