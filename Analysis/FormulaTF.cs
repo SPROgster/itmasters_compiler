2df5d173f8bc6e75581d6d96379cdea71dd02530
unknown <student@r315-4.mmcs.sfedu.ru>
2014-12-15 18:00:34 +0400

    Добавлена передаточная функция в виде формулы

---
+++ Analysis/FormulaTF.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SimpleLang.Analysis
+{
+    public class FormulaTransferFunction : InfoProvidedTransferFunction<List<Tuple<BitSet, BitSet>>, BitSet>
+    {
+        public FormulaTransferFunction(List<Tuple<BitSet, BitSet>> info)
+            : base(info)
+        { }
+
+        public override BitSet Transfer(BitSet input)
+        {
+            BitSet result = (BitSet) input.Clone();
+            foreach (var a in Info)
+            {
+                Console.WriteLine(result.ToString());
+                result = (BitSet)a.Item2.Union(result.Subtract(a.Item1));
+            }
+            return result;
+        }
+    }
+
+}
