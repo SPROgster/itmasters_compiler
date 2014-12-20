5cf7cab1f65ff9b65ad85675a7cb77c3c4e60837
Oleg <yurin.oleg.i@gmail.com>
2014-12-20 20:15:39 +0300

    Поиск естественных циклов исправил. Последняя вершина не попадала.

--- Analysis/NaturalCycles.cs
+++ Analysis/NaturalCycles.cs
@@ -12,9 +12,10 @@ namespace SimpleLang.Analysis
         public string GetLoop(ControlFlowGraph CFG, int n, int d)
         {
             List<BaseBlock> l = new List<BaseBlock>(CFG.GetBlocks());
-            List<int> l2=CFG.GetInputs(l[n+1]).Select(e => e.nBlock).ToList();
+            List<int> linputs = CFG.GetInputs(l[n + 1]).Select(e => e.nBlock).ToList();
             HashSet<int> h = new HashSet<int>();
-            foreach (int item in l2)
+            h.Add(n);
+            foreach (int item in linputs)
             {
                 h.Add(item);
                 DFS(CFG, l[item + 1], d, h);
@@ -25,12 +26,12 @@ namespace SimpleLang.Analysis
         public void DFS(ControlFlowGraph CFG, BaseBlock b, int d, HashSet<int> h)
         {
             List<BaseBlock> l = new List<BaseBlock>(CFG.GetBlocks());
-            List<int> l4 = CFG.GetInputs(l[b.nBlock+ 1]).Select(e => e.nBlock).ToList();
-            foreach (var item2 in l4)
+            List<int> linputs = CFG.GetInputs(l[b.nBlock+ 1]).Select(e => e.nBlock).ToList();
+            foreach (var item in linputs)
             {
-                h.Add(item2);
-                if (item2 == d) return;
-                DFS(CFG, l[item2 + 1], d, h);
+                h.Add(item);
+                if (item == d) return;
+                DFS(CFG, l[item + 1], d, h);
             }
         }
     }
