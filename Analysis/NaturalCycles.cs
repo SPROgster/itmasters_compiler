cbbf1135c0185ba9f2be680ccd28c21304390e88
Oleg <yurin.oleg.i@gmail.com>
2014-12-20 21:07:00 +0300

    Естественные циклы. Оптимизация.

--- Analysis/NaturalCycles.cs
+++ Analysis/NaturalCycles.cs
@@ -12,22 +12,15 @@ namespace SimpleLang.Analysis
         public string GetLoop(ControlFlowGraph CFG, int n, int d)
         {
             List<BaseBlock> l = new List<BaseBlock>(CFG.GetBlocks());
-            List<int> linputs = CFG.GetInputs(l[n + 1]).Select(e => e.nBlock).ToList();
             HashSet<int> h = new HashSet<int>();
             h.Add(n);
-            foreach (int item in linputs)
-            {
-                h.Add(item);
-                DFS(CFG, l[item + 1], d, h);
-                
-            }
+            DFS(CFG, l[n + 1], d, h);
             return String.Join(" ", h) ;
         }
         public void DFS(ControlFlowGraph CFG, BaseBlock b, int d, HashSet<int> h)
         {
             List<BaseBlock> l = new List<BaseBlock>(CFG.GetBlocks());
-            List<int> linputs = CFG.GetInputs(l[b.nBlock+ 1]).Select(e => e.nBlock).ToList();
-            foreach (var item in linputs)
+            foreach (var item in CFG.GetInputs(l[b.nBlock+ 1]).Select(e => e.nBlock).ToList())
             {
                 h.Add(item);
                 if (item == d) return;
